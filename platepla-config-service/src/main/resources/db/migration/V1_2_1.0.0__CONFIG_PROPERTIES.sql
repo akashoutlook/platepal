bf0bbb1044c1e7ffa979f7fb48b2f72ae0a112cf
Akash Debnath <akashdebnath@outlook.in>
2024-08-03 20:38:24 +0530

    Created platepal-config-service and platepal-discovery-service

---
+++ platepla-config-service/src/main/resources/db/migration/V1_2_1.0.0__CONFIG_PROPERTIES.sql
@@ -0,0 +1,5 @@
+-- platepal-config-service
+INSERT INTO PROPERTIES(APPLICATION, PROFILE, LABEL, KEY, VALUE) VALUES('platepal-config-service', 'jdbc', 'jdbc', 'server.port', '7001');
+
+-- platepal-discovery-service
+INSERT INTO PROPERTIES(APPLICATION, PROFILE, LABEL, KEY, VALUE) VALUES('platepal-discovery-service', 'jdbc', 'jdbc', 'server.port', '7002');
